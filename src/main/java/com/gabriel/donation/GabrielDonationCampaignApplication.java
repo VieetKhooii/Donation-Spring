6cd80a42627d6ce9b1cc493fe6b3abbb18c61092
VieetKhooii <nguyenkhooii03@gmail.com>
2024-09-13 10:13:33 +0700

    Initial commit

---
+++ src/main/java/com/gabriel/donation/GabrielDonationCampaignApplication.java
@@ -0,0 +1,14 @@
+package com.gabriel.donation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabrielDonationCampaignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GabrielDonationCampaignApplication.class, args);
+		System.out.println("http://localhost:8080");
+	}
+
+}
